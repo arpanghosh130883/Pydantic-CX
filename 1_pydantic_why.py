c8b266ad3cec8ab49583c8f68f0ebe60a32a8a93
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-06-01 00:12:58 +0530

    Update 1_pydantic_why.py

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -7,7 +7,7 @@ class Patient(BaseModel):
     email: EmailStr
     linkedin_url: AnyUrl
     age: int = Field(gt=0, lt=120)
-    weight: Annotated[float, Field(gt=0, strict=True)]
+    weight: Annotated[float, Field(gt=0, strict=True)] # strict = True is used to supress the property of pydantic to auto convert any numeric values to Int/float value if passed as string
     married: Annotated[bool, Field(default=None, description='Is the patient married or not')]
     allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str, str]
@@ -23,6 +23,32 @@ def update_patient_data(patient: Patient):
 
 patient_info = {'name':'nitish', 'email':'abc@gmail.com', 'linkedin_url':'http://linkedin.com/1322', 'age': '30', 'weight': 75.2,'contact_details':{'phone':'2353462'}}
 
+patient1 = Patient(**patient_info) # **patient_info unpacks the dictionary into individual keyword arguments matching the fields of the Patient class (which is a Pydantic model here).
+
+'''
+patient_info = {
+    'name': 'nitish',
+    'email': 'abc@gmail.com',
+    'linkedin_url': 'http://linkedin.com/1322',
+    'age': '30',
+    'weight': 75.2,
+    'contact_details': {'phone': '2353462'}
+}
+
 patient1 = Patient(**patient_info)
+
+will be unpacked to 
+
+Patient(
+    name='nitish',
+    email='abc@gmail.com',
+    linkedin_url='http://linkedin.com/1322',
+    age='30',
+    weight=75.2,
+    contact_details={'phone': '2353462'}
+)
+
+'''
+
 
 update_patient_data(patient1)
